82f5a3af1339004c14dacfc5cca7336377e15f80
Junghun Park <tacohun21@gmail.com>
2022-03-28 22:18:07 +0900

    netflix 앱 swiftUI 사용해서 리팩토링 하기

---
+++ NetfilxStyleSample/NetfilxStyleSample/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  NetfilxStyleSample
+//
+//  Created by Junghun Park on 2022/03/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let titles = ["Netflix Sample App"]
+    var body: some View {
+        NavigationView {
+            List(titles, id: \.self) {
+                let netflixVC = HomeViewControllerRepresentable()
+                    .navigationBarHidden(true)
+                    .edgesIgnoringSafeArea(.all)
+                NavigationLink($0, destination: netflixVC)
+            }
+            .navigationTitle("SwiftUI to UIKit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
