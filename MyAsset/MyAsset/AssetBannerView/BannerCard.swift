abbcf587f9410b5902ce294a8e2185438fa0e9f4
Junghun Park <tacohun21@gmail.com>
2022-04-08 15:41:50 +0900

    자산관리 앱 만들기 banner 만들기

---
+++ MyAsset/MyAsset/AssetBannerView/BannerCard.swift
@@ -0,0 +1,39 @@
+//
+//  BannerCard.swift
+//  MyAsset
+//
+//  Created by Junghun Park on 2022/04/08.
+//
+
+import SwiftUI
+
+struct BannerCard: View {
+    var banner: AssetBanner
+    
+    var body: some View {
+//        ZStack{
+//            Color(banner.backgroundColor)
+//            VStack{
+//                Text(banner.title)
+//                    .font(.title)
+//                Text(banner.description)
+//                    .font(.subheadline)
+//            }
+//        }
+        Color(banner.backgroundColor)
+            .overlay(
+                VStack{
+                    Text(banner.title)
+                        .font(.title)
+                    Text(banner.description)
+                        .font(.subheadline)
+                })
+    }
+}
+
+struct BannerCard_Previews: PreviewProvider {
+    static var previews: some View {
+        let banner0 = AssetBanner(title: "공지사항", description: "추가된 공지사항을 확인하세요", backgroundColor: .red)
+        BannerCard(banner: banner0)
+    }
+}
---
+++ MyAsset/MyAsset/Entities/AssetBanner.swift
@@ -0,0 +1,14 @@
+//
+//  AssetBanner.swift
+//  MyAsset
+//
+//  Created by Junghun Park on 2022/04/08.
+//
+
+import UIKit
+
+struct AssetBanner {
+    let title: String
+    let description: String
+    let backgroundColor: UIColor
+}
