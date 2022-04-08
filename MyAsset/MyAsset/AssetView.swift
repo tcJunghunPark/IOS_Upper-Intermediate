297e9d1c0fd031c4d21922f1c6837f566227abca
Junghun Park <tacohun21@gmail.com>
2022-04-08 15:06:25 +0900

    자산관리 앱 grid view로 버튼 만들기

---
+++ MyAsset/MyAsset/AssetView.swift
@@ -0,0 +1,29 @@
+//
+//  AssetView.swift
+//  MyAsset
+//
+//  Created by Junghun Park on 2022/04/08.
+//
+
+import SwiftUI
+
+struct AssetView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack (spacing: 30) {
+                    Spacer()
+                    AssetMenuGridView()
+                }
+            }
+            .background(Color.gray.opacity(0.2))
+            .navigationBarWithButtonStyle("내 자산")
+        }
+    }
+}
+
+struct AssetView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetView()
+    }
+}
